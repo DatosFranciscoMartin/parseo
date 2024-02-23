1e685681537aad2c7653fcda553efc99c1d08f81
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-02-23 13:28:32 +0100

    Se agrega una nueva forma de determinar el logo_branding en nuestro programa

--- programas/crear_xml.py
+++ programas/crear_xml.py
@@ -7,7 +7,7 @@ import tkinter as tk
 from tkinter import filedialog
 import logging
 
-media_id_espciales = ["P0*","P3*","A5*","P4*","P5*","P611PDS00049*","P699EDS00006*","P699EGS00121*","P611PGS00595*","P699PGS00568*","P699PGS00872*","P611PGS00547*","P899PGS00064*","P211PDS00033*","P211PDS00034*","P699PGS00961*","P211PDS00036*","P211EGS00006*","P531PGS00057*","P531PGS00056*","P211EGS00007*","P211PDS00037*","P211EGS00008*","P463PGS00057*","P211EGS00009*","P211PDS00038*","P469PDS00102*"]
+media_id_espciales = ["P0","P3","A5","P4","P5","P611PDS00049","P699EDS00006","P699EGS00121","P611PGS00595","P699PGS00568","P699PGS00872","P611PGS00547","P899PGS00064","P211PDS00033","P211PDS00034","P699PGS00961","P211PDS00036","P211EGS00006","P531PGS00057","P531PGS00056","P211EGS00007","P211PDS00037","P211EGS00008","P463PGS00057","P211EGS00009","P211PDS00038","P469PDS00102"]
 
 def seleccionar_directorio_salida():
     """
@@ -520,33 +520,45 @@ for archivo in lista_archivos:
             #A partir de aqui, creamos la condicion de que, si el campo TITIPELEME es A, D, E o P, se genera el branding
             if diccionario_interno["TITIPELEME"] in ["A","D","E","P"]:
                 # Las columnas de la tabla siguiente corresponde al Custom ID* | PT, SC or empty | NR7 | NR12 | NR16 |NR18 
-
                 tabla = {
                     'LA 1': ['L101', 'L106', 'L107', 'L112', 'L116', 'L118'],
                     'LA 2': ['L201', 'L206', 'L207', 'L212', 'L216', 'L218'],
-                    '24H': ['L401', 'L406', 'L407', 'L412', 'L416', 'L418'],
-                    'TDP': ['L702', 'L706', 'L707', 'L712', 'L716', 'L718'],
-                    'CLAN': ['L501', 'L506', 'L507', 'L512', 'L516', 'L518'],
-                    'STAR': ['L601', 'L606', 'L607', 'L612', 'L616', 'L618'],
+                    'CANAL 24H': ['L401', 'L406', 'L407', 'L412', 'L416', 'L418'],
+                    'TELEDEPORTE': ['L702', 'L706', 'L707', 'L712', 'L716', 'L718'],
+                    'CLAN TVE': ['L501', 'L506', 'L507', 'L512', 'L516', 'L518'],
+                    'Star TVE HD': ['L601', 'L606', 'L607', 'L612', 'L616', 'L618'],
                     'INTERNACIONALES': ['L301', 'L306', 'L307', 'L312', 'L316', 'L318']
                 }
 
+                # Logica que vamos a usar para determinar los canales que vienen a internacionales, que llevan nombre distintos
+                if LICADENA in ["TVE EUROPA-AFRICA", "TVE ASIA", "TVE AMERICA"]:
+                    LICADENA ="INTERNACIONALES"
+
                 # Distintas decisiones dependiendo de la calificacion moral que tenga, nombramos la variable columna para poder recorrer la tabla
-                if diccionario_interno["TICODELEMENMIN"] in media_id_espciales:
-                    columna = 0
-                elif diccionario_interno["CALIFMORAL"].rstrip() in ["PT","","SC"]:
-                    columna = 1
-                elif diccionario_interno["CALIFMORAL"].rstrip() == "NR7":
-                    columna = 2
-                elif diccionario_interno["CALIFMORAL"].rstrip() == "NR12":
-                    columna = 3
-                elif diccionario_interno["CALIFMORAL"].rstrip() == "NR16":
-                    columna = 4
-                elif diccionario_interno["CALIFMORAL"].rstrip() == "NR18":
-                    columna = 5
-                
                 # Una vez dicidida la columna, vamos a buscar el nombre del grafico en la tabla correspondiente, usando la variable licadena que nos indica el nombre del canal, y con la columna que nos indica la calificacion moral
-                logo_branding = tabla[LICADENA][columna]
+            
+            
+                # Agregamos una logica adicional para aquellos que tengan un media ID especial.
+                    
+                for prefix in media_id_espciales:
+                    if diccionario_interno["TICODELEMENMIN"].startswith(prefix):
+                        columna = 0
+                        logo_branding = tabla[LICADENA][columna]  
+                    elif diccionario_interno["CALIFMORAL"].rstrip() in ["PT","","SC"]:
+                        columna = 1
+                        logo_branding = tabla[LICADENA][columna]
+                    elif diccionario_interno["CALIFMORAL"].rstrip() == "NR7":
+                        columna = 2
+                        logo_branding = tabla[LICADENA][columna]
+                    elif diccionario_interno["CALIFMORAL"].rstrip() == "NR12":
+                        columna = 3
+                        logo_branding = tabla[LICADENA][columna]
+                    elif diccionario_interno["CALIFMORAL"].rstrip() == "NR16":
+                        columna = 4
+                        logo_branding = tabla[LICADENA][columna]
+                    elif diccionario_interno["CALIFMORAL"].rstrip() == "NR18":
+                        columna = 5
+                        logo_branding = tabla[LICADENA][columna]
 
                 # Generamos el arbol xml que va a colgar de childevents
                 event_child_1 = ET.SubElement(child_event, "event")
