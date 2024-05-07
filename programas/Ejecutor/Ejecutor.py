ddb1d8077ddeef4e2a44edf875be52622b013a4b
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-05-07 11:59:23 +0200

    Se han cambiado las decisiones a la hora de generar los grafismos

--- programas/Ejecutor/Ejecutor.py
+++ programas/Ejecutor/Ejecutor.py
@@ -603,25 +603,29 @@ def procesar_archivo(archivo, directorio_salida, origen_fichero):
                         schedule_child_5 = ET.SubElement(properties_child_5, "schedule")
 
                         # Si la hora de comienzo es T (duración total)
-                        if diccionario_tipo_3["HORA_DE_COMIENZO"].strip() == "T" and diccionario_tipo_3["TIPO_DE_INSERCION"] == "R" or diccionario_tipo_3["TIPO_DE_INSERCION"] == "Q":
-                            schedule_child_5.set("endType", "-ParentEnd")
-                            schedule_child_5.set("startType", "+ParentStart")
-                            schedule_child_5.set("endOffset", "00:00:00:00")
-                            schedule_child_5.set("startOffset", "00:00:12:00")
-
-                        elif diccionario_tipo_3["HORA_DE_COMIENZO"].strip() == "T" and diccionario_tipo_3["TIPO_DE_INSERCION"] != "R" or diccionario_tipo_3["TIPO_DE_INSERCION"] != "Q":
-                            schedule_child_5.set("endType", "-ParentEnd")
-                            schedule_child_5.set("startType", "+ParentStart")
-                            schedule_child_5.set("endOffset", "00:00:00:00")
-                            schedule_child_5.set("startOffset", "00:00:00:00")
+                        if diccionario_tipo_3["HORA_DE_COMIENZO"].rstrip()[0] == "T":
+                            if diccionario_tipo_3["TIPO_DE_INSERCION"] == "R" or diccionario_tipo_3["TIPO_DE_INSERCION"] == "Q":
+                                print("Condicion1")
+                                schedule_child_5.set("endType", "-ParentEnd")
+                                schedule_child_5.set("startType", "+ParentStart")
+                                schedule_child_5.set("endOffset", "00:00:00:00")
+                                schedule_child_5.set("startOffset", "00:00:12:00")
+                            elif diccionario_tipo_3["TIPO_DE_INSERCION"] != "R" or diccionario_tipo_3["TIPO_DE_INSERCION"] != "Q":
+                                print("Condicion2")
+                                schedule_child_5.set("endType", "-ParentEnd")
+                                schedule_child_5.set("startType", "+ParentStart")
+                                schedule_child_5.set("endOffset", "00:00:00:00")
+                                schedule_child_5.set("startOffset", "00:00:00:00")
 
                         elif diccionario_tipo_3["TIPO_DE_INSERCION"] == "S":
+                            print("Condicion3")
                             schedule_child_5.set("endType", "Duration")
                             schedule_child_5.set("startType", "+ParentStart")
                             schedule_child_5.set("endOffset", "00:00:08:00")
                             schedule_child_5.set("startOffset", "00:00:10:00")
                         
-                        elif diccionario_tipo_3["HORA_DE_COMIENZO"].strip() != "T" and diccionario_tipo_3["TIPO_DE_INSERCION"] == "P":
+                        elif diccionario_tipo_3["HORA_DE_COMIENZO"].rstrip()[0] != "T" and diccionario_tipo_3["TIPO_DE_INSERCION"] == "P":
+                            print("Condicion4")
                             schedule_child_5.set("endType", "-ParentEnd")
                             schedule_child_5.set("startType", "+ParentStart")
                             schedule_child_5.set("endOffset", "00:00:12:00")
