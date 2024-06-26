e2731c848fac57d90246ec4a5824928ec9c0cfff
Albertodogma <albertodogma@gmail.es>
2024-06-26 17:40:27 +0200

    Metemos el CODCINTA en los comentarios de los eventos que no sean PUBLI

--- programas/Ejecutor/Ejecutor.py
+++ programas/Ejecutor/Ejecutor.py
@@ -391,6 +391,8 @@ def procesar_archivo(archivo, directorio_salida, origen_fichero):
                     #print(RECONCILEKEY.replace(" ", "*"))
                     #event1_2 = ET.SubElement(properties1, "event")
                     event1_2.set("reconcileKey", RECONCILEKEY)
+                    comment1 = ET.SubElement(event1_2, "comment")
+                    comment1.text = diccionario_interno['Tipo2']["CODCINTA"].rstrip()
                 #print(RECONCILEKEY.replace(" ", "*"))
 
                 # Se comprueba si es tipo fijo o tipo secuencial
