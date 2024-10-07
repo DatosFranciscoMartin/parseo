b33d8b6e13d693345fe35fa1a390aeeb00ca4495
AlbertoMartinez <alberto.martinez@datosmedia.es>
2024-10-07 16:37:30 +0200

    Toda la B la coge del mediaName

--- programas/MARL_RGT(REGISTRATOR)/marl_rgt.py
+++ programas/MARL_RGT(REGISTRATOR)/marl_rgt.py
@@ -268,38 +268,27 @@ for archivo in lista_archivos:
                 A = DEFAULT[:11]
 
             try:
-                if reconcileKey is not None and Q != "5":
-
-                    # if  event.get('type') == "Live":
-                    #    B = reconcileKey[18:36]
-                    #    B = B[3:] + " "*3
-                    # else:
-                    if len(reconcileKey) == 45:
-                        # B = medianame
-                        B = reconcileKey[18:36]
-                        B = B[3:] + " " * 3
-                    elif len(reconcileKey) == 55:
-                        B = reconcileKey[17:22]
-                        B = "B" + B + " " * 12
+#                if reconcileKey is not None and Q != "5":
+#                    if len(reconcileKey) == 45:
+#                        # B = medianame
+#                        B = reconcileKey[18:36]
+#                        B = B[3:] + " " * 3
+#                    elif len(reconcileKey) == 55:
+#                        B = reconcileKey[17:22]
+#                        B = "B" + B + " " * 12
+#                    else:
+#                        B = DEFAULT[:18]
+#                else:
+                    # Busca el mediaName específicamente en la ruta event/properties/media
+                properties = event.find('properties')
+                if properties is not None:
+                    media = properties.find('media')
+                    if media is not None:
+                        B = media.get('mediaName')
                     else:
                         B = DEFAULT[:18]
                 else:
-                    #if event.get('type') == "Live":
-                    #    B = DEFAULT[:18]
-                    #elif medianame is not None and medianame != "$INHERIT$":
-                    #B = event.find('.//properties/media').get('mediaName')
-                    #    medianame = None
-
-                    # Busca el mediaName específicamente en la ruta event/properties/media
-                    properties = event.find('properties')
-                    if properties is not None:
-                        media = properties.find('media')
-                        if media is not None:
-                            B = media.get('mediaName')
-                        else:
-                            B = DEFAULT[:18]
-                    else:
-                        B = DEFAULT[:18]
+                    B = DEFAULT[:18]
             except AttributeError:
                 B = DEFAULT[:18]
 
