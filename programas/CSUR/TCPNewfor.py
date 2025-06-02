667dee68088eab95dc1b6999303aa3a272e281c1
AlbertoMartinez <alberto.martinez@datosmedia.es>
2025-06-03 01:47:44 +0200

    no es newfor, es prueba de sql

--- programas/CSUR/TCPNewfor.py
+++ programas/CSUR/TCPNewfor.py
@@ -5,33 +5,34 @@ server = '10.236.174.70'  # IP o nombre del servidor SQL remoto
 database = 'CanalSur_AutomationDB'
 username = 'pbsdbviewer'
 password = 'pbsdbviewer'
+driver = 'ODBC Driver 17 for SQL Server'  # O el que tengas disponible
 
-# Cadena de conexión
 conn_str = (
-    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
-    f'SERVER={server};'
-    f'DATABASE={database};'
-    f'UID={username};'
-    f'PWD={password}'
+    f"DRIVER={{{driver}}};"
+    f"SERVER={server};"
+    f"DATABASE={database};"
+    f"UID={username};"
+    f"PWD={password};"
+    f"TrustServerCertificate=yes;"
 )
 
 try:
-    # Conectar
-    with pyodbc.connect(conn_str) as conn:
-        cursor = conn.cursor()
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT [MediaName]
+        FROM [dbo].[taListMediaUsage]
+        WHERE [FUListActiveObjectId] = '5086'
+          AND [MediaSetId] = '8'
+          AND [MediaTypeId] = '0'
+    """)
+    for row in cursor.fetchall():
+        print(row.MediaName)
 
-        # Ejecutar consulta
-        cursor.execute("""
-            SELECT [MediaName]
-            FROM [dbo].[taListMediaUsage]
-            WHERE [MediaSetId] = '8' AND [MediaTypeId] = '0'
-        """)
-
-        # Obtener resultados
-        resultados = cursor.fetchall()
-
-        for fila in resultados:
-            print(fila.MediaName)
+    cursor.close()
+    conn.close()
 
 except Exception as e:
-    print("Error conectando o ejecutando la consulta:", e)
+    print("❌ Error:", e)
+
+input("\nPresiona ENTER para salir...")
